345b50844f7dde9ab407726819381763e8e3ebd0
EduClase <edudbdawclase@gmail.com>
2025-10-24 19:07:53 +0200

    Creando Factoria Comments

---
+++ app/Models/Comment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'text'
+    ];
+
+    protected $hidden = [
+        'user_id'
+    ];
+
+    public function user() : BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+}
